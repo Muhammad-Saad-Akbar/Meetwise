9b757869e71f8af74a66c4a77dfde61e97ee2244
SaadAkbarGithub <123985914+Muhammad-Saad-Akbar@users.noreply.github.com>
2026-02-09 22:34:52 +0500

    Chore: Initialize Meetwise project with Laravel + Vue starter kit

---
+++ resources/js/components/AppSidebar.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import NavFooter from '@/components/NavFooter.vue';
+import NavMain from '@/components/NavMain.vue';
+import NavUser from '@/components/NavUser.vue';
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+} from '@/components/ui/sidebar';
+import { dashboard } from '@/routes';
+import { type NavItem } from '@/types';
+import { LayoutGrid } from 'lucide-vue-next';
+
+const mainNavItems: NavItem[] = [
+    {
+        title: 'Dashboard',
+        href: dashboard(),
+        icon: LayoutGrid,
+    },
+];
+
+</script>
+
+<template>
+    <Sidebar collapsible="icon" variant="inset">
+
+        <SidebarContent>
+            <NavMain :items="mainNavItems" />
+        </SidebarContent>
+
+        <SidebarFooter>
+            <NavFooter :items="footerNavItems" />
+            <NavUser />
+        </SidebarFooter>
+    </Sidebar>
+    <slot />
+</template>
---
+++ resources/js/pages/Dashboard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { dashboard } from '@/routes';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: dashboard().url,
+    },
+];
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="bg-yellow-500 pt-10 pb-5 mt-14">
+            <h4 class="text-xl text-center font-bold">Welcome Saad</h4>
+        </div>
+    </AppLayout>
+<!--    <h1 class="text-red-500 text-2xl font-bold text-center">Saaad</h1>-->
+</template>
